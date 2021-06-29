c418b8fa5aa5d4a22ec2a935ef6e834176889b65
ROHAN <ro.anilpnr@gmail.com>
2021-06-29 21:06:47 +0530

    spin kit added

--- social_media_app/lib/screens/feed_screen.dart
+++ social_media_app/lib/screens/feed_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:social_media_app/constants.dart';
 import 'package:social_media_app/dummy_data.dart';
 import 'package:social_media_app/models/post.dart';
 import 'package:social_media_app/providers/api.dart';
 import 'package:social_media_app/widgets/post_item.dart';
 import 'package:social_media_app/widgets/app_bar.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class FeedScreen extends StatefulWidget {
   //const FeedScreen({ Key? key }) : super(key: key);
@@ -63,7 +65,12 @@ class _FeedScreenState extends State<FeedScreen> {
                             );
                       } else
                         return Center(
-                          child: Text('Loading'),
+                          child: Container(
+                            child: SpinKitWave(
+                              color: kPrimaryColor,
+                            ),
+                            height: MediaQuery.of(context).size.height * .8,
+                          ),
                         );
                     },
                   ),

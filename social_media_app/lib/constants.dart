1903be718c62e8f2a724e2a5ac6f16b7b7a9b9d1
ROHAN <ro.anilpnr@gmail.com>
2021-08-25 20:25:32 +0530

    font

--- social_media_app/lib/constants.dart
+++ social_media_app/lib/constants.dart
@@ -31,7 +31,7 @@ const LinearGradient kButtonLinearGradient = LinearGradient(
 const String kFontNamePrimary = 'Poppins';
 
 const kAppBarTitleStyle = TextStyle(
-  fontFamily: 'Billabong',
+  fontFamily: 'NovaSlim',
   // fontWeight: FontWeight.normal,
   fontSize: 30,
   color: Colors.white,

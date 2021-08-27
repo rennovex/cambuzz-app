575235ee2041c1fac31a514a2235e1ba0398cc62
ROHAN <ro.anilpnr@gmail.com>
2021-08-27 14:11:10 +0530

    Added privacy policy

--- social_media_app/lib/screens/Registration/intro.dart
+++ social_media_app/lib/screens/Registration/intro.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:social_media_app/providers/google_sign_in.dart';
 import 'package:provider/provider.dart';
 import 'package:social_media_app/screens/Registration/registration_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Intro extends StatelessWidget {
   final Function primaryButtonOnPressed;
@@ -48,6 +49,39 @@ class Intro extends StatelessWidget {
                   color: Color(0xff2f005f),
                   fontWeight: FontWeight.w500),
             ),
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Use your @tkmce.ac.in mail to login',
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('while logging in, you agree to our'),
+                SizedBox(
+                  width: 5,
+                ),
+                GestureDetector(
+                  onTap: (){
+                    launch('https://cambuzz.rennovex.com/privacy.html');
+                  },
+                    child: Text(
+                  'terms of use',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                ))
+              ],
+            )
           ],
         ),
       ),
